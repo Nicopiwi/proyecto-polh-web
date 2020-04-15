640b84cc03fe8f62e57d2777837ee292a728d67e
dylandevelopt <dylanszejnblum@gmail.com>
2020-04-15 20:47:26 -0300

    fixed read recipes

--- app/src/APIs.js
+++ app/src/APIs.js
@@ -1,18 +1,18 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "http://localhost:8080";
 
 const APIs = {
-    rest:{
-        loginMedico:`${BASE_URL}/medico/login`,
-        loginPaciente:`${BASE_URL}/paciente/login`,
-        loginFarmacia:`${BASE_URL}/farmacia/login`,
-        registerMedico:`${BASE_URL}/medico/register`,
-        registerPaciente:`${BASE_URL}/paciente/register`,
-        registerFarmacia:`${BASE_URL}/farmacia/register`,
-        createReceta:`${BASE_URL}/receta/issue`,
-        getRecetasMedico:`${BASE_URL}/receta/get-medic`,
-        transformHashToText:`${BASE_URL}/read-recipe/`
-    },
-    test:'https://randomuser.me/api/?results=1'
-}
+  rest: {
+    loginMedico: `${BASE_URL}/medico/login`,
+    loginPaciente: `${BASE_URL}/paciente/login`,
+    loginFarmacia: `${BASE_URL}/farmacia/login`,
+    registerMedico: `${BASE_URL}/medico/register`,
+    registerPaciente: `${BASE_URL}/paciente/register`,
+    registerFarmacia: `${BASE_URL}/farmacia/register`,
+    createReceta: `${BASE_URL}/receta/issue`,
+    getRecetasMedico: `${BASE_URL}/receta/get-medic`,
+    transformHashToText: `${BASE_URL}/receta/read-recipe/`,
+  },
+  test: "https://randomuser.me/api/?results=1",
+};
 
-export default APIs
+export default APIs;
