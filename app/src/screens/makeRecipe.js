9e1d858b4db0811deb64657e680d01bd13a22ecc
Nicopiwi <nikorozenberg@gmail.com>
2020-04-30 03:37:06 -0300

    Modificaciones para lo nuevo

--- app/src/screens/makeRecipe.js
+++ app/src/screens/makeRecipe.js
@@ -2,6 +2,12 @@ import React, {useState} from 'react';
 import './css/dashboard.css'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import IconButton from '@material-ui/core/IconButton';
+import Add from '@material-ui/icons/Add';
+import Delete from '@material-ui/icons/Delete';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 
 import { useDispatch, useSelector } from "react-redux";
@@ -13,9 +19,13 @@ import './css/dashboard.css'
 const useStyles = makeStyles((theme) =>
   createStyles({
     container:{
-        display:'flex',
-        flexWrap:'wrap'
+        //display:'flex',
+        flexWrap:'wrap',
     },
+    formControl: {
+        minWidth: 120,
+        marginTop:theme.spacing(2),
+      },
     spaced:{
         marginTop: theme.spacing(2),
         marginRight: theme.spacing(2)
@@ -23,11 +33,19 @@ const useStyles = makeStyles((theme) =>
   }),
 );
 
+
+
 const MakeRecipes = (props) =>{
     const classes = useStyles();
     const [recipeText, setRecipeText] = useState('');
+    const [medicamentosList, setMedicamentosList] = useState([{
+        nombreMedicamento:'', dosis:'', frecuencia:'', id:0
+    }])
+    const [duracionTratamiento, setDuracionTratamiento] = useState(null)
+    const [unitTratamiento, setUnitTratamiento] = useState(null)
     const [address, setAddress] = useState('');
     const [error, setError] = useState('');
+    const [onDelete, setOnDelete] = useState(false)
     const dispatch = useDispatch();
     const medicParams = useSelector(state => {
         return {
@@ -37,40 +55,159 @@ const MakeRecipes = (props) =>{
         }
     });
 
+    const values = (id, key)=>{
+        return undefined
+    }
+
     const handleSubmit = async ()=>{
         let headers = new Headers();
         headers.append('token', localStorage.getItem('userToken'));
         headers.append('Content-Type', 'application/json');
         headers.append('Accept', 'application/json');
-        let created = await dispatch(createRecipe(medicParams, {address: address}, recipeText))
+        let created = await dispatch(createRecipe(medicParams, {address: address}, 
+            JSON.stringify({
+                additionalNotes:recipeText,
+                medicamentos:medicamentosList,
+                duracion: `${duracionTratamiento} ${unitTratamiento}`
+            })))
         if (created){
             setError('')
             setRecipeText('')
             setAddress('')
+            resetMedicamentos()
             alert('Receta creada con éxito')
         }
         else{
             setError('Address inválida')
         }
     }
+
+    const resetMedicamentos=()=>{
+        setMedicamentosList([{
+            nombreMedicamento:'', dosis:'', frecuencia:'', id:0
+        }])
+    }
+
+    const handleAddMedicamento=()=>{
+        setMedicamentosList([...medicamentosList, {
+            nombreMedicamento:'', dosis:'', frecuencia:'', id:medicamentosList[medicamentosList.length-1].id+1
+        }])
+    }
+
+    const handleDeleteMedicamento=(i)=>{
+        setMedicamentosList(medicamentosList.filter((item, index)=>index!==medicamentosList.length-1))
+        setOnDelete(true)
+    }
+
+    const handleModifyMedicamento = (index, key, value)=>{
+        let aux = medicamentosList
+        switch(key){
+            case 'nombreMedicamento':
+                aux[index].nombreMedicamento =  value
+                setMedicamentosList(aux)
+                
+                break;
+            case 'dosis':
+                aux[index].dosis =  value
+                setMedicamentosList(aux)
+                break;
+            case 'frecuencia':
+                aux[index].frecuencia =  value
+                setMedicamentosList(aux)
+                break;
+            default:
+                return;
+        }
+    }
+
     return (
         <React.Fragment>
             <div className={classes.container}>
                 
+                {medicamentosList.map((item, index)=>{
+                    return(
+                        <div style={{display:'flex', flexDirection:'row'}}>
+                            <TextField
+                            
+                            onChange={(e)=>handleModifyMedicamento(index, 'nombreMedicamento', e.target.value)} 
+                            
+                            className={classes.spaced}
+                            label={`Nombre del medicamento ${index + 1}`} 
+                            variant="filled" 
+                            />
+                            <TextField
+                            
+                            onChange={(e)=>handleModifyMedicamento(index, 'dosis', e.target.value)} 
+                            className={classes.spaced}
+                            label={`Dosis ${index + 1}`} 
+                            variant="filled" 
+                            />
+                            <TextField
+                            
+                            onChange={(e)=>handleModifyMedicamento(index, 'frecuencia', e.target.value)} 
+                            className={classes.spaced}
+                            label={`Frecuencia ${index + 1}`} 
+                            variant="filled" 
+                            />
+                            
+                        </div>
+                    )
+                })}
+                <div className={classes.spaced} style={{display:'flex', flexDirection:'row'}}>
+                    <IconButton onClick={handleAddMedicamento} color="primary" aria-label="Add medicamento">
+                        <Add />
+                    </IconButton>
+                    {medicamentosList.length>1 && (
+                            <IconButton onClick={()=>handleDeleteMedicamento()} aria-label="Add medicamento">
+                                <Delete />
+                            </IconButton>)
+                    }
+                </div>
+                
+                
+                
+                <div  style={{display:'flex', flexDirection:'row'}}>
+                <TextField value={duracionTratamiento} 
+                    onChange={(e)=>setDuracionTratamiento(e.target.value)} 
+                    className={classes.spaced} id="duracionInputRecipe" 
+                    label="Duración del tratamiento" 
+                    variant="filled" 
+                    type="number"
+                    inputProps={{
+                        min:"0"
+                    }}
+                    />
+                    
+                    <FormControl variant="filled" className={classes.formControl}>
+                    <InputLabel htmlFor="filled-age-native-simple">Unidad</InputLabel>
+                    <Select
+                    native
+                    value={unitTratamiento}
+                    onChange={e=>setUnitTratamiento(e.target.value)}
+                    inputProps={{
+                        name: 'unidadTime',
+                        id: 'filled-age-native-simple',
+                    }}
+                    >
+                    <option value={'Días'}>Días</option>
+                    <option value={'Semanas'}>Semanas</option>
+                    <option value={'Meses'}>Meses</option>
+                    </Select>
+                </FormControl>
+                
+                </div>
                 <TextField
                 id="filled-multiline-static"
-                label="Escriba su receta aquí"
+                label="Notas adicionales"
                 multiline
-                placeholder="Maximo 500 caracteres"
                 fullWidth
-                rows={10}
+                rows={3}
                 maxLength={500}
                 variant="filled"
                 className={classes.spaced}
                 value={recipeText}
                 onChange={(e)=>setRecipeText(e.target.value)}
                 />
-
                 <TextField value={address} 
                 onChange={(e)=>setAddress(e.target.value)} 
                 className={classes.spaced} id="addressInputRecipe" 
--- app/src/screens/myRecipes.js
+++ app/src/screens/myRecipes.js
@@ -6,6 +6,7 @@ import { createStyles, makeStyles } from '@material-ui/core/styles';
 import './css/dashboard.css'
 import APIs from '../APIs';
 import Modal from '@material-ui/core/Modal';
+import { Typography } from '@material-ui/core';
 
 function getModalStyle() {
     const top = 50
@@ -43,24 +44,49 @@ const MyRecipes = (props) =>{
     const classes = useStyles();
     const [openModal, setOpenModal] = useState(false);
     const [recetaTexto, setRecetaTexto] = useState('');
+    const [recetaObj, setRecetaObj] = useState({});
+    const [hayReceta, setHayReceta] = useState(false);
+    const [loading, setLoading] = useState(false)
     const [modalStyle] = useState(getModalStyle);
+    const IsJsonString = (str)=>{
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
     const handleOpen = async (recipeText)=>{
       setOpenModal(true)
       let headers = new Headers();
       headers.append('token', localStorage.getItem('userToken'));
+      setRecetaTexto('Transcribiendo mensaje...')
+      setLoading(true)
       let text = await fetch(userType==='medico'?(APIs.rest.transformHashToText + recipeText):(APIs.rest.transformHashToTextFarmacia + recipeText), {method:'GET', headers})
       let textJson = await text.json()
       console.log(textJson)
       setRecetaTexto(textJson.result.message)
-      
+      if (IsJsonString(textJson.result.message)){
+        console.log('yeah')
+        let obj = await JSON.parse(textJson.result.message)
+        await setRecetaObj(Object.assign({}, obj))
+        setHayReceta(true)
+        //console.log(recetaAux)
+      }
+      else{
+          setHayReceta(false)
+      }
+      setLoading(false)
     }
     const handleClose = () => {
       setOpenModal(false);
     };
     useEffect(()=>{
         dispatch(fetchRecipes(userType))
-        console.log(recipesList)
-    }, [])
+        //console.log(recipesList)
+        console.log(hayReceta)
+    }, [hayReceta])
     
     return (
         <React.Fragment>
@@ -88,8 +114,47 @@ const MyRecipes = (props) =>{
                 aria-labelledby="receta-texto"
                 
             >
-                <textarea style={modalStyle} className={classes.paper} value={recetaTexto?recetaTexto:"Transcribiendo mensaje..."} disabled id="textoModal">
-                </textarea>
+                <div style={modalStyle} className={classes.paper}>
+                {/*
+                <textarea style={modalStyle} className={classes.paper} 
+                value={recetaTexto?`
+                    Medicamentos
+                    ${" "}
+                    ${JSON.parse(recetaTexto) && JSON.parse(recetaTexto).medicamentos.map((item, index)=>{
+                        return (item.nombreMedicamento + item.dosis + item.frecuencia)
+                    })}
+
+                    Duración del tratamiento
+                    ${" "}
+                    ${JSON.parse(recetaTexto) && JSON.parse(recetaTexto).duracion}
+
+                    Notas adicionales del médico
+                    ${" "}
+                    ${JSON.parse(recetaTexto) && JSON.parse(recetaTexto).additionalNotes}
+                `:"ERROR EN LA TRANSCRIPCIÓN DEL MENSAJE O RECETA VACÍA"} disabled id="textoModal">
+                </textarea>*/}
+                
+                {hayReceta?(<>
+                    <Typography variant="h6" style={{fontWeight:'600'}}>Medicamentos</Typography>
+                    {
+                        recetaObj.medicamentos.map((item, index)=>{
+                            return (
+                            <Typography>{index+1}- {item.nombreMedicamento}, {item.dosis}, {item.frecuencia}</Typography>
+                            )
+                        })
+                    }
+                    <br/>
+                    <br/>
+                    <Typography variant="h6" style={{fontWeight:'600'}}>Duración del tratamiento</Typography>
+                    <Typography>{recetaObj.duracion}</Typography>
+                    <br/>
+                    <br/>
+                    <Typography variant="h6" style={{fontWeight:'600'}}>Notas adicionales del médico</Typography>
+                    <Typography> {recetaObj.additionalNotes}</Typography>
+                </>):
+                <Typography>{loading?'Transcribiendo...':'ERROR EN LA TRANSCRIPCIÓN DEL MENSAJE O RECETA CON FORMATO ERRÓNEO'}</Typography>
+                }
+                </div>
             </Modal>
         </React.Fragment>
 
